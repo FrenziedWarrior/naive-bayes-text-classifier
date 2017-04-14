9d9057bc88e48f3bd8b9a1a0fa352d6270518932
Abhi Karmakar <abhi.karmakar604@gmail.com>
2017-04-13 18:21:24 -0700

    refactoring learning script

--- nb-learn.py
+++ nb-learn.py
@@ -3,46 +3,20 @@
 # i/p: dev-train-text.txt, dev-train-labels.txt
 
 # NO_OF_REVIEWS = 1280
-# TRAINING_SPLIT = 0.75
-# DEV_TEST_SPLIT = 0.25
 
 # 1 -> Read reviews line by line and perform tokenization (separate text into word tokens)
 #   a: Read corresponding labels assigned to the review and store required counts
 # 2 -> Maintain counts of term occurrences in documents of both classes in different data structures (dict)
 # 3 -> Count total number of word tokens in documents of all classes
 
-from math import log
+import math
 import sys
 import re
-import operator
-
-
-# dict_dt = {
-#     'deceptive': 0,
-#     'truthful': 0
-# }
-#
-# dt_term_count = {
-#     'deceptive': {},
-#     'truthful': {}
-# }
-#
-# dict_pn = {
-#     'positive': 0,
-#     'negative': 0
-# }
-#
-# pn_term_count = {
-#     'positive': {},
-#     'negative': {}
-# }
-
-# four_way_labels = ['deceptive positive', 'deceptive negative', 'truthful positive', 'truthful negative']
 
 
 class NaiveBayesClassifier(object):
     review_content = dict()
-    entire_vocab = list()
+    feature_vocab = list()
     stop_words = list()
 
     def __init__(self, labels):
@@ -50,6 +24,7 @@ class NaiveBayesClassifier(object):
         self.label_code = [label[0] for label in self.classes]   # label_code = ['d', 't']
         self.review_labels = dict()
         self.doc_count = {code: 0 for code in self.label_code}
+        self.doc_length = {code: 0 for code in self.label_code}
         self.term_count = {code: {} for code in self.label_code}
         self.priors = {code: 0.0 for code in self.label_code}
 
@@ -65,6 +40,23 @@ class NaiveBayesClassifier(object):
         curr_rev_label = self.review_labels[curr_id][0]
         for term in self.review_content[curr_id]:
             self.term_count[curr_rev_label].setdefault(term, 0) + 1
+
+    def calculate_priors(self):
+        sum_docs = sum(v for v in self.doc_count.values())
+        for k in self.priors.keys():
+            self.priors[k] = math.log(self.doc_count[k]/sum_docs)
+
+    def construct_vocabulary(self):
+        codes = self.label_code
+        for word in sorted(set(k for cl in codes for k in self.term_count[cl].keys())):
+            if sum(freq for freq in [class_dict.get(word, 0)
+                         for class_dict in self.term_count.values()]
+                   ) > 3:
+                self.feature_vocab.append(word)
+
+    def calculate_document_lengths(self):
+        for k in self.doc_length.keys():
+            self.doc_length[k] = sum(val for key, val in self.term_count[k].items() if key in self.feature_vocab)
 
 
 # retrieve list of stop words
@@ -120,136 +112,17 @@ def start_program():
         dt_filter.increment_doc_count(content_rev_id)
         dt_filter.update_term_freqs(content_rev_id)
 
+    dt_filter.calculate_priors()
+    dt_filter.construct_vocabulary()
+    dt_filter.calculate_document_lengths()
 
 start_program()
-# dec_prior = log(dict_dt['deceptive'] / (dict_dt['deceptive'] + dict_dt['truthful']))
-# tru_prior = log(dict_dt['truthful'] / (dict_dt['deceptive'] + dict_dt['truthful']))
-# pos_prior = log(dict_pn['positive'] / (dict_pn['positive'] + dict_pn['negative']))
-# neg_prior = log(dict_pn['negative'] / (dict_pn['positive'] + dict_pn['negative']))
-#
-# dec_word_list = dt_term_count['deceptive']
-# tru_word_list = dt_term_count['truthful']
-# pos_word_list = pn_term_count['positive']
-# neg_word_list = pn_term_count['negative']
 
-# sorted_freq1 = dict(sorted(dec_word_list.items(), key=operator.itemgetter(1), reverse=True)[:15])
-# sorted_freq2 = dict(sorted(tru_word_list.items(), key=operator.itemgetter(1), reverse=True)[:15])
-# sorted_freq3 = dict(sorted(pos_word_list.items(), key=operator.itemgetter(1), reverse=True)[:15])
-# sorted_freq4 = dict(sorted(neg_word_list.items(), key=operator.itemgetter(1), reverse=True)[:15])
-# print(sorted_freq1)
-# print(sorted_freq2)
-# print(sorted_freq3)
-# print(sorted_freq4)
-
-# list_vocab = set(
-#     list(dec_word_list.keys()) + list(tru_word_list.keys())
-# )
-#
-# for word in list_vocab:
-#     if word in dec_word_list:
-#         dec_token = True
-#         rc_dec = dec_word_list[word]
-#     else:
-#         dec_token = False
-#         rc_dec = 0
-#
-#     if word in tru_word_list:
-#         tru_token = True
-#         rc_tru = tru_word_list[word]
-#     else:
-#         tru_token = False
-#         rc_tru = 0
-#
-#     # removing rare tokens
-#     if rc_tru + rc_dec < 3:
-#         if dec_token:
-#             dec_word_list.pop(word)
-#         elif tru_token:
-#             tru_word_list.pop(word)
-#
-# # counting length of documents of each class
-# len_dec_docs = 0
-# len_tru_docs = 0
-# len_pos_docs = 0
-# len_neg_docs = 0
-#
-# for key, val in dec_word_list.items():
-#     len_dec_docs += val
-#
-# for key, val in tru_word_list.items():
-#     len_tru_docs += val
-#
-# for key, val in pos_word_list.items():
-#     len_pos_docs += val
-#
-# for key, val in neg_word_list.items():
-#     len_neg_docs += val
-#
-# sorted_vocab = sorted(set(
-#     list(dec_word_list.keys()) + list(tru_word_list.keys())
-# ))
+# import operator: use in sorted() to sort dictionary by values
+# to find n most frequently occuring words in each class
+# sorted_freq1 = dict(sorted(class_word_list.items(), key=operator.itemgetter(1), reverse=True)[:15])
 #
 # for word in sorted_vocab:
-#     # Word Token Type
-#     target_model.write(word + ' ')
-#     #    print("%20s" % word, end='\t\t')
-#
-#     # Word Token raw count - (deceptive)
-#     if word in dec_word_list:
-#         target_model.write(str(dec_word_list[word]) + ' ')
-#     # print(str(dec_word_list[word]), end=' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#     # print(str(0), end=' ')
-#
-#     # Word Token raw count - (truthful)
-#     if word in tru_word_list:
-#         target_model.write(str(tru_word_list[word]) + ' ')
-#     # print(str(tru_word_list[word]), end=' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#     # print(str(0), end=' ')
-#
-#     # Word Token raw count - (positive)
-#     if word in pos_word_list:
-#         target_model.write(str(pos_word_list[word]) + ' ')
-#     # print(str(pos_word_list[word]), end=' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#     # print(str(0), end=' ')
-#
-#     # Word Token raw count - (negative)
-#     if word in neg_word_list:
-#         target_model.write(str(neg_word_list[word]) + ' ')
-#     # print(str(neg_word_list[word]))
-#     else:
-#         target_model.write(str(0) + ' ')
-#     # print(str(0))
-#
-#     # P(Tk/C) probability - (deceptive)
-#     if word in dec_word_list:
-#         target_model.write(str(log(dec_word_list[word] / len_dec_docs)) + ' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#
-#     # P(Tk/C) probability - (truthful)
-#     if word in tru_word_list:
-#         target_model.write(str(log(tru_word_list[word] / len_tru_docs)) + ' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#
-#     # P(Tk/C) probability - (positive)
-#     if word in pos_word_list:
-#         target_model.write(str(log(pos_word_list[word] / len_pos_docs)) + ' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#
-#     # P(Tk/C) probability - (negative)
-#     if word in neg_word_list:
-#         target_model.write(str(log(neg_word_list[word] / len_neg_docs)) + ' ')
-#     else:
-#         target_model.write(str(0) + ' ')
-#
 #     # log P(Tk/C) probability after smoothing - (deceptive)
 #     if word in dec_word_list:
 #         target_model.write(str(log((dec_word_list[word] + 1) / (len_dec_docs + len(sorted_vocab)))) + ' ')
